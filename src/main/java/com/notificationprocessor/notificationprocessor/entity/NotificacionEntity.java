df4e19b68167dbad2943e0f2a7301f5b5c20def4
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2025-02-05 18:03:38 -0500

    .

--- src/main/java/com/notificationprocessor/notificationprocessor/entity/NotificacionEntity.java
+++ src/main/java/com/notificationprocessor/notificationprocessor/entity/NotificacionEntity.java
@@ -1,7 +1,6 @@
 package com.notificationprocessor.notificationprocessor.entity;
 
 import jakarta.persistence.*;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
